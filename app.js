6f860ffdac5cca59b06b64f9a66d2df5d9edd0af
dhananjeyal <46560287+dhananjeyal@users.noreply.github.com>
2019-11-20 16:40:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use( compression() )
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, './dist', 'index.html'));
 });
-const PORT = 3001;
+const PORT = 3004;
 
 console.log(PORT);
 
